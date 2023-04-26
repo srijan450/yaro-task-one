b6825a86bdb35d4e445b6780a6a9433f3471726e
srijan shankar dubey <srijanshankardubey99@gmail.com>
2023-04-27 01:35:52 +0530

    changed database url

--- Database/connection.js
+++ Database/connection.js
@@ -2,12 +2,19 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 console.log(process.env.DB_USER);
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dglfs.mongodb.net/?retryWrites=true&w=majority`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.9e9fifr.mongodb.net/?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
     console.log("connected to database");
-}).catch((e) => {
+  })
+  .catch((e) => {
     console.log("cannot connect to database due to following error");
     console.log(e);
-});
+  });

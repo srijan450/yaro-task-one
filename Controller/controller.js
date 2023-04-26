bc600bf68edd196b3cd05b4092ad1a43fa097582
srijan shankar dubey <srijanshankardubey99@gmail.com>
2023-04-27 01:46:33 +0530

    added link to repo

--- Controller/controller.js
+++ Controller/controller.js
@@ -1,41 +1,50 @@
 import Model from "../Models/model.js";
 import bcryptjs from "bcryptjs";
 
+export const information = async (req, res) => {
+  res.send(
+    '<a href="https://github.com/srijan450/yaro-task-one">Link To Repo</a>'
+  );
+};
+
 export const sign = async (req, res) => {
-    const requested = Object.keys(req.body);
-    const allowed = ["mobile", "password"];
-    const valid = requested.every((item) => allowed.includes(item));
+  const requested = Object.keys(req.body);
+  const allowed = ["mobile", "password"];
+  const valid = requested.every((item) => allowed.includes(item));
 
-    if (!valid || requested.length !== 2)
-        return res.status(400).json({ error: "Invalid request to the server" });
-    try {
-        const { mobile, password } = req.body;
-        const user = await Model.findByCredentials(req.body);
-        if (!user) {
-            const hashedPassword = await bcryptjs.hash(password, 8);
-            const newUser = await Model({ mobile, password: hashedPassword });
-            const token = await newUser.genrateToken();
-            res.cookie('token', token, { httpOnly: true, });
-            return res.status(201).json({ success: true, message: "registration successful" });
-        }
-        const token = await user.genrateToken();
-        res.cookie('token', token, { httpOnly: true, });
-        res.status(201).json({ success: true, message: "login successful" });
-
-    } catch (e) {
-        if (e.code === 11000) {
-            return res.status(404).json({ success: false, message: "incorrect password" });
-        }
-        res.status(500).json({ error: "internal server error" });
+  if (!valid || requested.length !== 2)
+    return res.status(400).json({ error: "Invalid request to the server" });
+  try {
+    const { mobile, password } = req.body;
+    const user = await Model.findByCredentials(req.body);
+    if (!user) {
+      const hashedPassword = await bcryptjs.hash(password, 8);
+      const newUser = await Model({ mobile, password: hashedPassword });
+      const token = await newUser.genrateToken();
+      res.cookie("token", token, { httpOnly: true });
+      return res
+        .status(201)
+        .json({ success: true, message: "registration successful" });
     }
-}
+    const token = await user.genrateToken();
+    res.cookie("token", token, { httpOnly: true });
+    res.status(201).json({ success: true, message: "login successful" });
+  } catch (e) {
+    if (e.code === 11000) {
+      return res
+        .status(404)
+        .json({ success: false, message: "incorrect password" });
+    }
+    res.status(500).json({ error: "internal server error" });
+  }
+};
 
 export const page = (req, res) => {
-    res.status(200).json({ message: "hello form srijan shankar dubey" });
-}
+  res.status(200).json({ message: "hello form srijan shankar dubey" });
+};
 
 export const signOut = (req, res) => {
-    req.user.token = "";
-    req.user.save();
-    res.status(201).json({ success: true, message: "sign-out successful" });
-}
+  req.user.token = "";
+  req.user.save();
+  res.status(201).json({ success: true, message: "sign-out successful" });
+};
--- Routes/routes.js
+++ Routes/routes.js
@@ -1,8 +1,9 @@
-import { Router } from 'express';
-import { authenticate } from '../Authentication/auth.js';
-import { page, sign, signOut } from '../Controller/controller.js';
+import { Router } from "express";
+import { authenticate } from "../Authentication/auth.js";
+import { page, sign, signOut, information } from "../Controller/controller.js";
 
 const Route = Router();
+Route.get("/", information);
 Route.post("/sign", sign);
 Route.get("/test-page", authenticate, page);
 Route.post("/sign-out", authenticate, signOut);
